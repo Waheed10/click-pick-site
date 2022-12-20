c7873a058465d02e2a5628833dc614e78199a48e
saeed <saeed@SAEED>
2022-12-20 21:07:32 +0500

    Add project files.

---
+++ website/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using website.Models;
+
+namespace website.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult cont()
+        {
+            return View();
+        }
+
+    }
+}
